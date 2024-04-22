c335e30b4087c5760572f743117fbac0a995c502
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-22 16:48:21 +0200

    not found

--- BookStore.Api/Program.cs
+++ BookStore.Api/Program.cs
@@ -33,8 +33,13 @@ List<BookDto> books = [
 app.MapGet("books", () => books);
 
 // GET /books/1
-app.MapGet("books/{id}", (int id) => books.Find(book => book.Id == id))
-    .WithName(GetGameEndpointName);
+app.MapGet("books/{id}", (int id) =>
+{
+    BookDto? book = books.Find(book => book.Id == id);
+
+    return book is null ? Results.NotFound() : Results.Ok(book);
+})
+.WithName(GetGameEndpointName);
 
 // POST /books
 app.MapPost("books", (CreateBookDto newBook) =>
